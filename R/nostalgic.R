ceb216afe39d2a299d7549cad527127083029989
rsquaredin <rsquaredin@gmail.com>
2018-11-08 16:19:00 +0530

    add pirate translation

---
+++ R/nostalgic.R
@@ -0,0 +1,23 @@
+#' Pirate translator
+#'
+#' Translate from English to Pirate.
+#'
+#' @param text Text to be translated.
+#'
+#' @examples
+#' \dontrun{
+#' translate_pirate("What nonsense is this?")
+#' }
+#'
+#' @export
+#'
+translate_pirate <- function(text) {
+
+  if (pingr::is_online()) {
+    prep_translate(text, "pirate")
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+}
+
--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -6,4 +6,10 @@ httptest::with_mock_api({
       translate_piglatin("Pig Latin is used in schools to teach language constructs."),
       "ig-Pay atin-Lay is-way used-way in-way ools-schay o-tay each-tay anguage-lay onstructs-cay. ")
   })
+
+  test_that("pirate translation works", {
+    expect_equivalent(
+      translate_pirate("Hello sir! my mother goes with me to the ocean."),
+      "Ahoy matey, by Blackbeard's sword!  me dear ol' mum, bless her black soul goes with me t' th' briny deep.")
+  })
 })
