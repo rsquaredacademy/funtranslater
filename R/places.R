68b3f3540387df0bce5cc4486ad6facaa6ba0377
rsquaredin <rsquaredin@gmail.com>
2018-11-09 12:11:27 +0530

    add norfolk translation

--- R/places.R
+++ R/places.R
@@ -89,3 +89,26 @@ translate_boston <- function(text) {
   }
 
 }
+
+#' Norfolk translator
+#'
+#' Translate from English to Norfolk dialect.
+#'
+#' @param text Text to be translated.
+#'
+#' @examples
+#' \dontrun{
+#' translate_norfolk("What nonsense is this?")
+#' }
+#'
+#' @export
+#'
+translate_norfolk <- function(text) {
+
+  if (pingr::is_online()) {
+    prep_translate(text, "norfolk")
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+}
