78cf00c8061be25bf3c08f7f8bfff41f11dcfd68
rsquaredin <rsquaredin@gmail.com>
2018-11-09 12:25:54 +0530

    add wowslang translation

--- R/internet-fads.R
+++ R/internet-fads.R
@@ -20,3 +20,26 @@ translate_emoji <- function(text) {
   }
 
 }
+
+#' Wowslang	 converter
+#'
+#' Convert from English to World of Warcraft slang. 
+#'
+#' @param text Text to be converted.
+#'
+#' @examples
+#' \dontrun{
+#' translate_wowslang("What nonsense is this?")
+#' }
+#'
+#' @export
+#'
+translate_wowslang <- function(text) {
+
+  if (pingr::is_online()) {
+    prep_translate(text, "wowslang")
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+}
